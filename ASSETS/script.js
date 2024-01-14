6d7cce8f9cbd96ef6989dac78d1ae273fb8b8964
Cody T <crthorn11@gmail.com>
2024-01-14 13:07:12 -0500

    added optimized code for the timer, and submit button to save scores

--- ASSETS/script.js
+++ ASSETS/script.js
@@ -3,46 +3,46 @@ const questions = [
     {
         question: "Commonly used data types DO NOT include",
         answers: [
-            { text: "alerts", correct: true},
-            { text: "strings", correct: false},
-            { text: "booleans", correct: false},
-            { text: "numbers", correct: false},
+            { text: "alerts", correct: true },
+            { text: "strings", correct: false },
+            { text: "booleans", correct: false },
+            { text: "numbers", correct: false },
         ]
     },
     {
         question: "The condition in an if / else statement is enclosed within ___.",
         answers: [
-            { text: "quotes", correct: true},
-            { text: "curly brackets", correct: false},
-            { text: "parenthises", correct: false},
-            { text: "square brackets", correct: false},
+            { text: "quotes", correct: true },
+            { text: "curly brackets", correct: false },
+            { text: "parenthises", correct: false },
+            { text: "square brackets", correct: false },
         ]
     },
     {
         question: "Arrays in JavaScript can be used to store ___.",
         answers: [
-            { text: "numbers and strings", correct: false},
-            { text: "other arrays", correct: false},
-            { text: "booleans", correct: true},
-            { text: "all of the above", correct: false},
+            { text: "numbers and strings", correct: false },
+            { text: "other arrays", correct: false },
+            { text: "booleans", correct: true },
+            { text: "all of the above", correct: false },
         ]
     },
     {
         question: "String values must be enclosed within ___ when being assigned to variables.",
         answers: [
-            { text: "commas", correct: false},
-            { text: "curly bracket", correct: false},
-            { text: "quotes", correct: true},
-            { text: "parenthises", correct: false},
+            { text: "commas", correct: false },
+            { text: "curly bracket", correct: false },
+            { text: "quotes", correct: true },
+            { text: "parenthises", correct: false },
         ]
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: [
-            { text: "JavaScript", correct: false},
-            { text: "terminal/bash", correct: false},
-            { text: "for loops", correct: false},
-            { text: "console log", correct: true},
+            { text: "JavaScript", correct: false },
+            { text: "terminal/bash", correct: false },
+            { text: "for loops", correct: false },
+            { text: "console log", correct: true },
         ]
     },
 ];
@@ -53,69 +53,77 @@ const nextButton = document.getElementById("next-btn");
 const startBtn = document.getElementById("start-btn");
 const quizScreen = document.getElementById("quiz-screen");
 const startScreen = document.getElementById("start-screen");
-const startingMinutes = 1;
-let time = startingMinutes * 60;
-const countdownEl = document.getElementById("countdown");
+let time = 6;
+const countdownEl = document.getElementById("countDown");
+const inputDiv = document.getElementById("initial-screen");
+const initialButton = document.getElementById("initial-btn");
+const initialForm = document.getElementById("initial-form");
+
+initialButton.addEventListener("click", function (event) {
+    saveScore(event);
+});
+
 
 let currentQuestionIndex = 0;
-let score = 0; 
+let score = 0;
+let intervalHolder;
 
 
 
-function startQuiz(){
+function startQuiz() {
     startScreen.classList.add("hide");
     quizScreen.classList.remove("hide");
-    //goodplace for timer here and should be done next, then try focusing on questions next
     showQuestion();
-    setInterval(updateCountdown, 1000);
+    intervalHolder = setInterval(updateCountdown, 1000);
 }
-function updateCountdown(){
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+function updateCountdown() {
+    time--;
 
-        seconds = seconds < 1 ? "0" + seconds : seconds;
-        
-        countdownEl.innerHTML = `${minutes}:${seconds}`;
-        time--;
-    }
+   if(time <= 0) {
+    clearInterval(intervalHolder);
+    showScore();
+   }
+
+    countdownEl.innerHTML = time;
+}
 
 function showQuestion() {
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.
-    question;
+        question;
 
     currentQuestion.answers.forEach(answer => {
-const button = document.createElement("button");
-button.innerHTML = answer.text;
-button.classList.add("btn");
-answerButtons.appendChild(button);
-if(answer.correct){
-    button.dataset.correct = answer.correct;
-}
-button.addEventListener("click", selectAnswer);
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 }
 
-function resetState(){
+function resetState() {
     nextButton.style.display = "none";
-    while(answerButtons.firstChild){
-    answerButtons.removeChild(answerButtons.firstChild);
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
     }
 }
 
-function selectAnswer(e){
+function selectAnswer(e) {
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
-    if(isCorrect){
+    if (isCorrect) {
         selectedBtn.classList.add("correct");
         score++;
-    }else{
+    } else {
         selectedBtn.classList.add("incorrect");
     }
     Array.from(answerButtons.children).forEach(button => {
-        if(button.dataset.correct === "true"){
+        if (button.dataset.correct === "true") {
             button.classList.add("correct");
         }
         button.disabled = true;
@@ -123,26 +131,53 @@ function selectAnswer(e){
     nextButton.style.display = "block";
 }
 
-function showScore(){
+function showScore() {
     resetState();
-    questionElement.innerHTML = "You scored ${score} out of ${questions.length}!";
-    nextButton.innerHTML = "play again";
-    nextButton.style.display = "block";
+    
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!  Enter your initials to save your score`;
+    inputDiv.removeAttribute("class");
 }
 
-function handleNextButton(){
+function saveScore(event) {
+    event.preventDefault();
+    var input = document.getElementById("initial-input").value;
+    var userScore = {
+        initials: input,
+        score: score
+    };
+    console.log(userScore);
+    var scores = JSON.parse(localStorage.getItem("scores")) || [];
+    scores.push(userScore);
+    localStorage.setItem("scores", JSON.stringify(scores));
+    displayScores();
+}
+
+function displayScores() {
+var savedScores = localStorage.getItem("scores") || [];
+console.log(savedScores);
+}
+
+initialButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    startScreen.classList.remove("hide");
+    quizScreen.classList.add("hide");
+    inputDiv.classList.add("hide");
+    initialForm.classList.add("hide");
+  });
+
+function handleNextButton() {
     currentQuestionIndex++;
-    if(currentQuestionIndex < questions.length){
+    if (currentQuestionIndex < questions.length) {
         showQuestion();
-    }else{
+    } else {
         showScore();
     }
 }
 
-nextButton.addEventListener("click", ()=>{
-    if(currentQuestionIndex < questions.length){
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
         handleNextButton();
-    }else{
+    } else {
         startQuiz();
     }
 });
